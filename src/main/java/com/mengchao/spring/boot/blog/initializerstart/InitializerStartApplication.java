ff1a7511495afff5fc87386cbddef3d3b8e2ab72
JoJo <mengchaoj@student.unimelb.edu.au>
2019-08-05 17:07:43 +1000

    initialization

---
+++ src/main/java/com/mengchao/spring/boot/blog/initializerstart/InitializerStartApplication.java
@@ -0,0 +1,13 @@
+package com.mengchao.spring.boot.blog.initializerstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerStartApplication.class, args);
+	}
+
+}
